a587a73d8f2570f4f66027c3e513c0f45c3f2d8f
Nathalya <160874305+Nathalyagsn@users.noreply.github.com>
2024-04-15 18:57:09 -0300

    Create ex012.py
    Exercício Python 12: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.

---
+++ ex012.py
@@ -0,0 +1,4 @@
+P = float (input('Digite o preço do produto pare receber o desconto de 5% : '))
+dis = (5 / 100) * P
+print ('O produto agora custa: R$ {} '.format(P - dis))
+print ('Seu desconto foi de: R$ {}'.format(dis))
